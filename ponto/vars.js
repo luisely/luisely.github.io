45fb8d7a5cbfed96c7732b7e0256bd63a88c3e6f
Fernando Ely <luisely@gmail.com>
2025-07-29 23:18:16 -0300

    FIX - version 1.9

--- ponto/vars.js
+++ ponto/vars.js
@@ -1,7 +1,7 @@
 export const BUTTON_WITH_LOADING = `<svg class="animate-spin h-5 w-5 mr-2 inline-block text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                                     <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                                     <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"></path>
-                                    </svg> Registrando...`
+                                    </svg>`
 
 export const toastSuccess = (message) => (Toastify({
                 text: message,

9ae8674f421f3b969a2a95a085bc0a0cf40ee3b6
Fernando Ely <luisely@gmail.com>
2026-01-08 18:41:38 -0300

    aumenta tamanho fonte e ajuste tamanho container.

--- ponto/src/renderTabelaPontos.js
+++ ponto/src/renderTabelaPontos.js
@@ -32,12 +32,27 @@ export function renderTabelaPontos(pontos) {
 			const totalHHMM = formatMinutesToHHMM(totalMinutes)
 
 			html += `<div>`
-			html += `<div class="text-center text-lg font-bold tracking-wider rounded-t bg-stone-500/15 border-b border-b-black/85 dark:border-b-white/65 dark:text-stone-300 text-black suse ">${date} •${new Date(year, month - 1, day).toLocaleDateString('pt-BR', { weekday: 'short' })} • <span data-less8h=${isLess8h} data-plus8h=${isPlus8h} data-ok=${isOk} class="data-[less8h='true']:dark:text-amber-400 data-[less8h='true']:text-amber-700 data-[plus8h='true']:dark:text-pink-400 data-[plus8h='true']:text-pink-600">${totalHHMM}${isPlus8h ? '!' : ''}</span></div>`
+			html += `<div class="text-center text-base h-8 font-bold tracking-wider rounded-t dark:bg-stone-900 bg-stone-500/15 border-b border-b-black/85 
+							dark:border-b-white/65 dark:text-stone-300 text-black suse flex justify-between items-center px-2">
+							${date} - ${new Date(year, month - 1, day).toLocaleDateString('pt-BR', { weekday: 'short' })}
+								<span 
+									data-less8h=${isLess8h} 
+									data-plus8h=${isPlus8h} 
+									data-ok=${isOk} 
+									class=" data-[less8h='true']:dark:text-amber-400 data-[less8h='true']:text-amber-700 data-[plus8h='true']:dark:text-pink-400 data-[plus8h='true']:text-pink-600"
+								>
+									🕛 ${totalHHMM}${isPlus8h ? '!' : ''}
+								</span>
+							</div>`
 			html += `<div class="flex justify-between px-1 py-1 mb-1 rounded-b dark:bg-blue-900/75 bg-blue-500">`
 			porData[date]
 				.sort() // Ordena horários
 				.forEach((time) => {
-					html += `<div class="text-md mx-2"><a href="#" class="link-delete hover:text-stone-950 dark:text-white suse text-sm transition" data-record="${date}&${time}");">•${time}</a></div>`
+					html += `<div class="text-md mx-2">
+									 	<a href="#" class="link-delete hover:text-blue-400 dark:text-white suse text-lg transition" data-record="${date}&${time}");">
+									 		•${time}
+									 	</a>
+									 </div>`
 				})
 			html += `</div>`
 			html += `</div>`
